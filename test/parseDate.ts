e70fd87fc530dea27dc12a8dae614f48110aff3c
ngryman <ngryman@gmail.com>
2020-08-13 10:38:05 +0200

    test: test the parser caching mechanism

--- test/parseDate.ts
+++ test/parseDate.ts
@@ -1,4 +1,5 @@
 import test, { Macro } from 'ava'
+import { performance, PerformanceObserver } from 'perf_hooks'
 import parseDate from '../src/parseDate'
 import { FormatOptions } from '../src/types'
 
@@ -12,3 +13,17 @@ const date = new Date(Date.UTC(1984, 0, 17, 16, 13, 37, 0))
 test('parse a date', testDate, date)
 test('parse a date with a custom locale', testDate, date, { locale: 'fr' })
 test('parse a date with a custom timezone', testDate, date, { timezone: 'Asia/Singapore' })
+
+test.cb('cache the parser given the same options', t => {
+  const minotiredParseDate = performance.timerify(parseDate)
+  const obs = new PerformanceObserver(list => {
+    const [first, second] = list.getEntries()
+    t.true(second.duration < first.duration)
+    obs.disconnect()
+    t.end()
+  })
+  obs.observe({ entryTypes: ['function'], buffered: true })
+
+  minotiredParseDate(date, { locale: 'es' })
+  minotiredParseDate(date, { locale: 'es' })
+})
